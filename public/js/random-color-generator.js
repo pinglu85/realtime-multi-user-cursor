9e9ab4cf2f2a80e9b68fcc3abd22a7d60f311168
pinglu85 <luping8576@hotmail.com>
2020-03-28 18:53:00 +0100

    delete cursor color

--- public/js/random-color-generator.js
+++ public/js/random-color-generator.js
@@ -1,16 +1,7 @@
 let userIconsArray = [].slice.call(document.querySelectorAll('.user-icon'));
-let cursorIconsArray = [].slice.call(document.querySelectorAll('.cursor-icon'));
 
 userIconsArray.forEach(userIcon => {
   userIcon.style.background = `rgb(${Math.floor(
     Math.random() * 255
   )}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-});
-
-cursorIconsArray.forEach(cursorIcon => {
-  let r = Math.floor(Math.random() * 255);
-  let g = Math.floor(Math.random() * 255);
-  let b = Math.floor(Math.random() * 255);
-  cursorIcon.style.stroke = `rgb(${r}, ${g}, ${b})`;
-  cursorIcon.nextElementSibling.style.background = `rgba(${r}, ${g}, ${b}, 0.7)`;
 });
