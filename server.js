19b155eafaf8a154502ce534adf6a04bd4357afe
pinglu85 <luping8576@hotmail.com>
2020-03-28 18:57:53 +0100

    add listen for mouse position & broadcast client's mouse position

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when client connects
+io.on('connection', socket => {
+  // Listen for mouse position
+  socket.on('mousePos', pos => {
+    // Broadcast client's mouse position
+    socket.broadcast.emit('mousePos', pos);
+  });
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
